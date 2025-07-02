4e9186cf9cd46c0e7ce31bfca58382ad623aa3ba
sbx_user1051 <sbx_user1051@169.254.3.17>
2025-07-02 17:36:45 +0000

    Wed Jul 02 17:36:45 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,13 +2,18 @@
 class SumSolution:
     
     def compute(self, x, y):
-        check_valid_input(input)
+        check_valid_input(x)
+        check_valid_input(y)
         return x + y
 
-    def check_valid_input(input):
-        valid_input = 0 <= input <= 100
-        
-        
+
+def check_valid_input(input):
+    valid_input = 0 <= input <= 100
+    if not valid_input:
+        raise Exception(f"Invalid input {input}. Inputs must be between 0 and 100")
+
+    
+
 
 
 
