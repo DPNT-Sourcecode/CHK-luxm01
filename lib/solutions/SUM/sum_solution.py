d6fd181783f1a9c8ab35fbc2f73de88f13d3bf01
sbx_user1051 <sbx_user1051@169.254.3.17>
2025-07-02 17:33:45 +0000

    Wed Jul 02 17:33:45 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,9 +2,12 @@
 class SumSolution:
     
     def compute(self, x, y):
+        check_valid_input(input)
         return x + y
 
     def check_valid_input(input):
+        valid_input = 0 <= input <= 100
+        
         
 
 
--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -5,8 +5,7 @@ class TestSum():
     def test_sum(self):
         assert SumSolution().compute(1, 2) == 3
 
-    def test_sum(self):
+    def test_sum_raises(self):
         with pytest.raises(Exception) as e:
             SumSolution().compute(101, -1)
-        
 
