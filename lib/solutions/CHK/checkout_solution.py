ee8fd1ad571d8ddcc69a14f9ef949dc67e8739a5
sbx_user1051 <sbx_user1051@169.254.6.207>
2025-07-03 08:33:48 +0000

    Thu Jul 03 08:33:48 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,17 +4,20 @@ class CheckoutSolution:
     def checkout(self, skus):
         try:
             for sku in skus:
-                print(f"*********SKU {sku} ************")
                 validate_checkout(sku)
+                print(f"{}")
         except:
+            print(f"IN EXCEPT BLOCK{skus}")
             return -1
-        return 50
+
         
 
 def validate_checkout(sku):
     valid_inputs = ["A", "B", "C", "D"]
     if sku not in valid_inputs:
         raise Exception(f"Invalid input: {input}. Valid inputs = {valid_inputs}")
+    return True
+
 
 
 
