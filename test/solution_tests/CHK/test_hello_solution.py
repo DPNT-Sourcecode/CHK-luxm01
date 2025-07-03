92304432862f95e5eb084f91a48c8770dc91611b
sbx_user1051 <sbx_user1051@169.254.6.207>
2025-07-03 08:00:48 +0000

    Thu Jul 03 08:00:48 UTC 2025

--- test/solution_tests/CHK/test_hello_solution.py
+++ test/solution_tests/CHK/test_hello_solution.py
@@ -6,7 +6,15 @@ class TestCheckout:
     @pytest.mark.parametrize(
         "input, expected",
         [
-            ("", ""),
+            ("A", 50),
+            ("AA", 100)
+            ("AAA", 130),
+            ("AAAA", 180),
+            ("AAAAAA", 260)
+            ("B", 30),
+            ("BB", 45),
+            ("BBB", 75),
+            
         ],
     )
     def test_checkout_doesnt_raise(self, input, expected):
@@ -19,7 +27,9 @@ class TestCheckout:
         ],
     )
     def test_checkout_raises(self, input):
+        # unhappy path returns -1
         with pytest.raises(Exception):
             CheckoutSolution().checkout(input)
+
 
 
