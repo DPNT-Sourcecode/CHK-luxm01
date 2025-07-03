72f6decedf1e388f4ee1c6768f8e87a9cf556037
sbx_user1051 <sbx_user1051@169.254.6.207>
2025-07-03 09:18:48 +0000

    Thu Jul 03 09:18:48 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,12 @@
 import copy
 
 PRICE_TABLE_AND_OFFERS = {
-    "A": {"price": 50, "offer": {3: 130}},
-    "B": {"price": 30, "offer": {2: 45}},
+    "A": {"price": 50, "offer": {"group_size": 3, "price_per_group": 130}},
+    "B": {"price": 30, "offer": {"group_size": 2, "price_per_group": 45}},
     "C": {"price": 20, "offer": {}},
     "D": {"price": 15, "offer": {}},
 }
+
 
 class CheckoutSolution:
     # skus = unicode string
@@ -23,21 +24,23 @@ class CheckoutSolution:
         except Exception as e:
             print(f"********* FAILED WITH {e} *********")
             return -1
-        
+
+
 def validate_checkout(sku):
     valid_inputs = ["A", "B", "C", "D"]
     if sku not in valid_inputs:
         raise Exception(f"Invalid input: {input}. Valid inputs = {valid_inputs}")
     return True
 
+
 def get_total_price(shopping_cart_dict):
     total = 0
     for sku, number_of_items in shopping_cart_dict.items():
-        multiplier = copy.deepcopy(number_of_items) # deepcopy might be overkill
+        multiplier = copy.deepcopy(number_of_items)  # deepcopy might be overkill
         offer = PRICE_TABLE_AND_OFFERS.get(sku).get("offer")
         if offer:
-            group_size = offer.keys()[0]
-            price_per_group = offer.values()[0]
+            group_size = offer.get("group_size")
+            price_per_group = offer.get("price_per_group")
             number_of_groups = int(multiplier / group_size)
             total += number_of_groups * price_per_group
             multiplier = multiplier % group_size
