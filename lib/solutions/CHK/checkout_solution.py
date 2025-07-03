ab51b7524c1dfe36ceeb482b42fde21416f3e786
sbx_user1051 <sbx_user1051@169.254.6.207>
2025-07-03 12:04:47 +0000

    Thu Jul 03 12:04:47 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -88,7 +88,10 @@ def get_total_price(shopping_cart_dict):
     for sku, number_of_items in shopping_cart_dict.items():
         if sku in BUY_N_SKUS_FOR_P["skus"]:
             relevant_skus[sku] = {"quantity": number_of_items, "price": PRICE_TABLE_AND_OFFERS[sku]["price"]}
-    relevant_skus_sorted_by_price = dict(sorted(relevant_skus.items(), key=lambda item: item[1]['price']))
+    relevant_skus_sorted_by_price = dict(sorted(relevant_skus.items(), key=lambda item: item[1]['price'], reverse=True))
+    
+    for sku, quantity_price in relevant_skus_sorted_by_price:
+
     if relevant_skus:
         print(f"\n******************* relevant_skus_sorted_by_price:\n{relevant_skus_sorted_by_price}")
 
@@ -131,6 +134,7 @@ def get_total_price(shopping_cart_dict):
                     multiplier = multiplier % group_size
         total += PRICE_TABLE_AND_OFFERS[sku]["price"] * multiplier
     return total
+
 
 
 
