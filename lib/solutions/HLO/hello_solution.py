967fa539be4927ab5b4696db6b249d6cf3cbeddd
sbx_user1051 <sbx_user1051@169.254.6.207>
2025-07-03 07:41:40 +0000

    Thu Jul 03 07:41:40 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,13 +1,15 @@
-
 class HelloSolution:
-    
+
     # friend_name = unicode string
     def hello(self, friend_name):
         validate_unicode_string(friend_name)
         return f"Hello, {friend_name}!"
-    
+
+
 # Helpers
 def validate_unicode_string(input):
     if not isinstance(input, str):
-        raise Exception(f"Invalid input: {input}. Valid inputs must be unicode strings.")
+        raise Exception(
+            f"Invalid input: {input}. Valid inputs must be unicode strings."
+        )
 
--- test/solution_tests/HLO/test_hello_solution.py
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -1,27 +1,30 @@
 from solutions.HLO.hello_solution import HelloSolution
 import pytest
 
-class TestHello():
+
+class TestHello:
     def test_hello(self):
         assert HelloSolution().hello("Matt") == "Hello, Matt!"
-    
-    @pytest.mark.parametrize("input",
-    [
-        (1),
-    ]
+
+    @pytest.mark.parametrize(
+        "input",
+        [
+            (1),
+        ],
     )
     def test_hello_raises(self, input):
         with pytest.raises(Exception):
             HelloSolution().hello(input)
 
-    @pytest.mark.parametrize("input, expected",
+    @pytest.mark.parametrize(
+        "input, expected",
         [
             ("Chloë", "Hello, Chloë!"),
             ("Jean-Paul", "Hello, Jean-Paul!"),
             ("5", "Hello, 5!"),
-        ]
-
+        ],
     )
     def test_hello_doesnt_raise(self, input, expected):
         assert HelloSolution().hello(input) == expected
+
 
